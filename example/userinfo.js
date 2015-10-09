2bf8529c3ead98b492bf1ce6241340e81884646c
you21979 <you2197901@gmail.com>
2015-10-09 19:07:23 +0900

    mod

--- example/userinfo.js
+++ example/userinfo.js
@@ -7,4 +7,5 @@ var apikey = CONFIG.apikey;
 var secret = CONFIG.secret;
 var api = okcoin.createPrivateApi(apikey, secret, "okcoin/trade agent");
 api.spot.userInfo().then(console.log)
+api.future.userInfo().then(console.log)
 
