f29c80152890bfe9e03fe59cc8e6740630c76c9d
you21979 <you2197901@gmail.com>
2015-10-09 18:58:19 +0900

    add error code

--- example/userinfo.js
+++ example/userinfo.js
@@ -1,6 +1,10 @@
 var okcoin = require('..');
+var fs = require("fs");
 
-var apikey = "";
-var secret = "";
+var CONFIG = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+
+var apikey = CONFIG.apikey;
+var secret = CONFIG.secret;
 var api = okcoin.createPrivateApi(apikey, secret, "okcoin/trade agent");
 api.spot.userInfo().then(console.log)
+
