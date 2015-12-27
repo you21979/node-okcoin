9e10a04927fcb85f9a11704b2f0ce9f2f331d314
you2 <you2197901@gmail.com>
2015-12-28 02:52:58 +0900

    mod

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 exports.Constant = require('./lib/constant');
+exports.FutureConstant = require('./lib/future_constant');
+exports.SpotConstant = require('./lib/spot_constant');
 exports.PublicApi = require('./lib/public_api');
 exports.createPrivateApi = require('./lib/private_api');
