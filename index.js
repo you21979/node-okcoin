45186d337c2bd9fe27a9062f6c51a14875ca6b55
you21979 <you2197901@gmail.com>
2018-06-19 19:23:33 +0900

    exclude websocket

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ exports.FutureConstant = require('./lib/future_constant');
 exports.SpotConstant = require('./lib/spot_constant');
 exports.PublicApi = require('./lib/public_api');
 exports.createPrivateApi = require('./lib/private_api');
-exports.WebinfoApi = require('./lib/webinfo_api'); // undocument api
